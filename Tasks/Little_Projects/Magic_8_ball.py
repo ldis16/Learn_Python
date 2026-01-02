05861a64f92cde77ee01df7311da1ed7adb4b47d
ldis16 <learn.inter.16@gmail.com>
2026-01-02 03:07:07 +0200

    adjusting Magic 8 ball game to requirements

--- Tasks/Little_Projects/Magic_8_ball.py
+++ Tasks/Little_Projects/Magic_8_ball.py
@@ -2,7 +2,7 @@ import random
 """Описание проекта: магический шар 8 (шар судьбы) — шуточный способ предсказывать будущее. 
 Программа должна просить пользователя задать некий вопрос, чтобы случайным образом на него ответить."""
 
-def asking_ball(question):
+def choise(question):
     responses = ['Даже не думай', 
                  'Мой ответ - нет', 
                  'По моим данным - нет', 
@@ -30,9 +30,45 @@ def asking_ball(question):
     
     return message
 
+def is_correct_name(name):
+    i_name = str(name)
+    flag = True
+    if i_name.isdigit() or i_name == '':
+        flag = False
+    return flag
+
+def is_new_question(answer):
+    if answer.lower() == 'да':
+        return True
+    return False
+
 def main():
-    question = input('Задайте ваш вопрос: ')
-    answer = asking_ball(question)
-    print('Шар отвечает вам - {0}'.format(answer))
+    greeting_message = 'Привет Мир, я магический шар, и я знаю ответ на любой твой вопрос.'
+    name_question = 'Пожалуйста назови свое имя'
+    name = ''
+    ask_for_question_masseage = 'Задайте ваш вопрос: '
+
+    print(greeting_message)
+    print(name_question)
+
+    while not is_correct_name(name):
+        name = input()
+    print('Привет, {0}'.format(name))
+    
+    working_flag = True
+    while working_flag:
+        print(ask_for_question_masseage)
+        question = input()
+        answer = choise(question)
+    
+        print('Шар отвечает вам - {0}'.format(answer))
+        print()
+        print('{0}, Хотите задать ещё один вопрос?'.format(name))
+
+        answer = input()
+        working_flag = is_new_question(answer)
+    
+    print('Возвращайся, если возникнут вопросы!')
+        
 
 main()
