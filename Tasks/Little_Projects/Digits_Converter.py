bede22a818c0f0dc24855389dc12cb7ef93aeb1e
ldis16 <learn.inter.16@gmail.com>
2026-01-10 03:13:34 +0200

    optimization of Digits Converter

--- Tasks/Little_Projects/Digits_Converter.py
+++ Tasks/Little_Projects/Digits_Converter.py
@@ -1,14 +1,18 @@
 """
 Simple digits converter from dex to another system by base and vice-versa
 """
-
+import random
 
 def main():
     # tests
-    # print(num_to_dex_by_base_string('111111', 2))
-    # print(num_to_dex_by_base_string('1AF2', 16))
-    # print(find_base(['88', '32', '22', '16', '17']))
-    # print(dex_to_num_by_base_string('1000', 16))
+    base = 1616
+    print("Base:", base)
+    num = 161616161616
+    print("num:", num)
+    rnd_base_num = dex_to_num_by_base_string(num, base)
+    print("new num:", rnd_base_num)
+    reverse_num = num_to_dex_by_base_string(rnd_base_num, base)
+    print("reverse num:", reverse_num)
     ...
 
 
@@ -36,41 +40,30 @@ def find_base(seq: list) -> str:
 def num_to_dex_by_base_string(hex_num: str, base: int) -> str:
     int_res = 0
     inv_hex = hex_num[::-1]
-    if base <= (36): # If the base is within the limit of English letters in lowercase + 10
+    if base <= 36:  # If the base is within the limit of English letters in lowercase + 10
         inv_hex = inv_hex.lower()
-    letters = get_additional_letter_by_base(base)
 
     for i in range(len(hex_num)):
         n = inv_hex[i]
         n_num = 0
-        if n in letters:
-            n_num = 10 + letters.index(n)
-        else:
+        if n.isdecimal():
             n_num = int(n)
+        else:
+            n_num = 10 + ord(n) - ord("a")
+            
         int_res += n_num * base**i
 
     return str(int_res)
 
 
-def get_additional_letter_by_base(base: int) -> str:
-    letters = ""
-    add_letters = base - 10
-
-    if add_letters > -1:
-        for i in range(add_letters):
-            letters += chr(ord("a") + i)
-    return letters
-
-
 def dex_to_num_by_base_string(dex_num: str, base: int) -> str:
     result = ""
-    letters = get_additional_letter_by_base(base)
     int_dex_num = int(dex_num)
     while int_dex_num > 0:
         num = int_dex_num % base
         int_dex_num = int_dex_num // base
         if num > 9:
-            num = letters[num - 10]
+            num = chr(ord("a") + (num - 10))
         result += str(num)
     return result[::-1]
 
